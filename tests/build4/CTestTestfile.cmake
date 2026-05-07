e05b96d08114ff1fe2ea82692821ca2424071ae5
cuihaitao <chuihairu@outlook.com>
2026-05-07 17:29:37 +0800

    chore: 彻底清理测试构建产物

--- tests/build4/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/workspaces/wingman/tests/build4
-# Build directory: D:/workspaces/wingman/tests/build4
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/workspaces/wingman/tests/build4/minimal_tests[1]_include.cmake")
-subdirs("googletest")
